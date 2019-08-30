db0d11aee28191186bf4a6ffbc59b799a947bc83
void-zero <wallace.lucena@hotmail.com>
2019-08-30 19:45:14 -0300

    Fixed RVND metaheuristic

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,16 @@
 #include "readData.h"
 #include "TSP.h"
 
-double ** matrizAdj; // matriz de adjacencia
-int dimension; // quantidade total de vertices
-
 int main(int argc, char** argv) {
+    double ** matrizAdj; // matriz de adjacencia
+    int dimension; // quantidade total de vertices
+    
     readData(argc, argv, &dimension, &matrizAdj);
-        srand(time(NULL));
+
     TSP tsp = TSP(&matrizAdj, dimension);
-    tsp.printMatrix();
+
+    if(dimension < 16)
+        tsp.printMatrix();
 
     std::cout << "\nTotal cost: " << tsp.getCost() << "\nReal cost: " << tsp.getRealCost() << std::endl;
     tsp.printSolution();
